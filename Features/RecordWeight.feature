1747b0ca0652567f298aa27f8da643b0f14443b1
djtcarrilho <djtcarrilho@hotmail.com>
2016-06-15 16:45:47 +0100

    cleaning things

---
+++ Features/RecordWeight.feature
@@ -0,0 +1,10 @@
+﻿Feature: RecordWeight
+	In order to avoid silly mistakes
+	As a math idiot
+	I want to be told the sum of two numbers
+
+@mytag
+Scenario: record my Weight
+	Given I have entered 50 kilos as my weight on 10/06/2016
+	When I press save
+	Then the system should display my current weight as 50 kilos
---
+++ Features/RecordWeightSteps.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace Features
+{
+    [Binding]
+    public class RecordWeightSteps
+    {
+        [Given(@"I have entered (.*) kilos as my weight on (.*)/(.*)")]
+        public void GivenIHaveEnteredKilosAsMyWeightOn(int p0, string p1, int p2)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I press save")]
+        public void WhenIPressSave()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the system should display my current weight as (.*) kilos")]
+        public void ThenTheSystemShouldDisplayMyCurrentWeightAsKilos(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}

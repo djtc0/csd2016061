57acb4cf35d3aa96af03cf5236dbbc0d6c9eb351
djtcarrilho <djtcarrilho@hotmail.com>
2016-06-15 15:46:57 +0100

    packages

---
+++ Features/Calculator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Features
+{
+    internal class Calculator
+    {
+        public List<int> values { get; set; }
+        public int result { get; set; }
+        public Calculator()
+        {
+            result = 0;
+            values = new List<int>();
+        }
+        
+        public void sumAllValues()
+        {
+            result = 0;
+            foreach (var value in values)
+            {
+                result += value;
+            }
+        }
+    }
+}
---
+++ Features/SpecFlowFeature1Steps.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+using TechTalk.SpecFlow;
+
+namespace Features
+{
+    [Binding]
+    public class SpecFlowFeature1Steps
+    {
+        private Calculator calculator = new Calculator();
+
+        [Given(@"I have entered (.*) into the calculator")]
+        
+        public void GivenIHaveEnteredIntoTheCalculator(int p0)
+        {
+            //ScenarioContext.Current.Pending();
+            calculator.values.Add(p0);
+        }
+        
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+             calculator.sumAllValues();
+        }
+        
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int p0)
+        {
+            //ScenarioContext.Current.Pending();
+            Assert.AreEqual(calculator.result, p0);
+        }
+    }
+}
